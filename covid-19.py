7edd1df052ef0f323b0889c3f98e1987d8a08e5c
Arca Ege Cengiz <arcaegecengiz@gmal.com>
2020-03-25 18:55:29 +0000

    Bug fix

--- covid-19.py
+++ covid-19.py
@@ -1,6 +1,7 @@
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 
+
 def post_data():
     all = requests.get('https://coronavirus-19-api.herokuapp.com/all')
     cases_all = all.json()['cases']
@@ -8,8 +9,9 @@ def post_data():
     recovered_all = all.json()['recovered']
     requests.post('https://api.thingspeak.com/update?api_key=Y9VNTK6O6MF0ZAEB&field1={}&field2={}&field3={}'.format(str(cases_all), str(recovered_all), str(deaths_all)))
 
+
 scheduler = BackgroundScheduler()
 job = scheduler.add_job(post_data, 'interval', seconds=30)
 scheduler.start()
-while 1==1:
+while 1 == 1:
     pass
